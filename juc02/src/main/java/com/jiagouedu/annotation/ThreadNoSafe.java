df1b6177f7ce2226ddbd7820945b0194f4889066
dafeige <364583707@qq.com>
2019-05-08 21:15:37 +0800

    重构多线程学习

--- juc02/src/main/java/com/jiagouedu/annotation/ThreadNoSafe.java
+++
@@ -1,8 +0,0 @@
-package com.jiagouedu.annotation;
-
-
-/***
- * 非线程安全的
- */
-public @interface ThreadNoSafe {
-}
---
+++ thread-pool/src/main/java/org/thread/runable/RunableTest.java
@@ -0,0 +1,38 @@
+package org.thread.runable;
+
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RunableTest implements  Runnable {
+
+    //private Map<String,Object> tempMap = new HashMap<>();
+    private ConcurrentHashMap tempMap = new ConcurrentHashMap();
+    private Integer value = null;
+
+    @Override
+    public void run() {
+        try{
+            System.out.println("this="+Thread.currentThread().getName()+" -"+this);
+            value = new Random().nextInt(10);
+            tempMap.put("value",value);
+            System.out.println("当前线程放入的值"+Thread.currentThread().getName()+"---"+tempMap);
+            Thread.sleep(10);
+            System.out.println("runnable....线程+"+Thread.currentThread().getName()+"---"+tempMap);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String[] arg0) throws Exception{
+        Runnable able = new RunableTest();
+        Thread aa = new Thread(able);
+        aa.start();
+        aa.wait();
+        aa.join();
+        Thread.yield();
+        new Thread(able).start();
+        new Thread(able).start();
+        new Thread(able).start();
+    }
+}
