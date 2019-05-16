5accb87663c8f115734584d5b90c86c14f6014af
dafeige <364583707@qq.com>
2019-05-16 21:39:01 +0800

    collection 单链表 完成

--- java-collection/src/main/java/org/java/collection/javacollection/LinkTable/SingleLinkTable.java
+++ java-collection/src/main/java/org/java/collection/javacollection/LinkTable/SingleLinkTable.java
@@ -1,5 +1,6 @@
 package org.java.collection.javacollection.LinkTable;
 
+import com.alibaba.fastjson.JSON;
 import org.java.collection.javacollection.node.Node;
 
 import java.io.Serializable;
@@ -34,6 +35,8 @@ public class SingleLinkTable<E> implements Serializable {
        size++;
    }
 
+
+
     /**
      * 根据下标获取指定元素
      * @param index
@@ -49,6 +52,79 @@ public class SingleLinkTable<E> implements Serializable {
    }
 
     /**
+     * 设置某一个节点的值
+     * @param index
+     * @param e
+     */
+   public void set(int index,E e){
+       checkIndex(index);
+       Node node = root.getNext();
+       for(int i = 0; i < index; i++){
+           node = node.getNext();
+       }
+       node.setData(e);
+   }
+
+    /**
+     * 移除节点
+     * @param index
+     */
+   public void remove(int index){
+       checkIndex(index);
+       Node node = root.getNext();
+       if(index == 0){
+           Node node2 = node.getNext();
+           root.setNext(node2);
+           node = null;
+       }else if(index == (size - 1)){
+           for(int i = 0; i < index - 1; i++){
+               node = node.getNext();
+           }
+           Node lastNode = node.getNext();
+           node.setNext(null);
+           lastNode = null;
+       }else{
+           for(int i = 0; i < index - 1; i++){
+               node = node.getNext();
+           }
+           Node indexNode = node.getNext();
+           Node indexNodeNext = indexNode.getNext();
+           node.setNext(indexNodeNext);
+           indexNode = null;
+       }
+       size -- ;
+   }
+
+    /**
+     * 插入节点
+     * @param index
+     */
+   public void insert(int index, E e){
+       checkIndex(index);
+       Node node = root.getNext();
+       if(index == 0){
+           Node newNode = new Node(e);
+           if(null != node){
+               newNode.setNext(node);
+           }
+           root.setNext(newNode);
+       }else if(index == size){
+           for(int i = 0; i < index - 1; i++){
+               node = node.getNext();
+           }
+           Node newNode = new Node(e);
+           node.setNext(newNode);
+       }else{
+           for(int i = 0; i < index - 1; i++){
+               node = node.getNext();
+           }
+           Node newNodexNext = node.getNext();
+           Node newNode = new Node(e);
+           newNode.setNext(newNodexNext);
+           node.setNext(newNode);
+       }
+   }
+    /**
      * 检查下标Index是否合法
      */
     private void checkIndex(int index) {
@@ -61,13 +137,37 @@ public class SingleLinkTable<E> implements Serializable {
         return "Index: " + index + ", Size: " + size;
     }
 
+    @Override
+    public String toString() {
+        return "SingleLinkTable{" +
+                "root=" + root +
+                '}';
+    }
+
     public static void main(String[] arg0){
         SingleLinkTable table = new SingleLinkTable();
-        for(int i = 0; i < 1; i++){
-            table.addNode(i);
+        for(int i = 0; i < 10; i++){
+            table.addNode(i+1);
         }
 
-        System.out.println(table.get(0));
+        /*System.out.println("移除第一个="+table.get(0));
+        table.remove(0);
+        System.out.println("移除第一个="+table.get(0));*/
+       /* System.out.println("移除最后一个="+table.get(9));
+        System.out.println("移除最后一个="+table.get(8));
+        table.remove(9);
+        System.out.println("移除最后一个="+table.get(8));*/
+        /*System.out.println("第5个="+table.get(4));
+        table.remove(4);
+        System.out.println("移除第5个="+table.get(4));
+        System.out.println("移除第5个="+table.get(3));*/
+
+        System.out.println("插入5之前="+table.get(8));
+        System.out.println("插入5之前="+table.get(9));
+        table.insert(10,20);
+        System.out.println("插入5之后="+table.get(8));
+        System.out.println("插入5之后="+table.get(9));
+        System.out.println("插入5之后="+table.get(10));
     }
 
 
